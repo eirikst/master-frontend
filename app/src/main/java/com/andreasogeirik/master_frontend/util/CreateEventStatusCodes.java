b2d98b95e9601ddeb93dccde86f0f7ca78eb30e9
Andreas Naess <andreas.n92@gmail.com>
2016-02-24 13:09:04 +0100

    Refactor validation

---
+++ app/src/main/java/com/andreasogeirik/master_frontend/util/CreateEventStatusCodes.java
@@ -0,0 +1,7 @@
+package com.andreasogeirik.master_frontend.util;
+
+/**
+ * Created by Andreas on 24.02.2016.
+ */
+public enum CreateEventStatusCodes {
+}
---
+++ app/src/main/java/com/andreasogeirik/master_frontend/util/CreateEventValidationContainer.java
@@ -0,0 +1,32 @@
+package com.andreasogeirik.master_frontend.util;
+
+import com.andreasogeirik.master_frontend.util.CreateEventStatusCodes;
+
+/**
+ * Created by Andreas on 24.02.2016.
+ */
+public class CreateEventValidationContainer {
+    private CreateEventStatusCodes statusCode;
+    private String error;
+
+    public CreateEventValidationContainer(CreateEventStatusCodes statusCode, String error) {
+        this.statusCode = statusCode;
+        this.error = error;
+    }
+
+    public CreateEventStatusCodes getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(CreateEventStatusCodes statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
